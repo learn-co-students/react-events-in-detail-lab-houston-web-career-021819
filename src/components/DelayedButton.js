2b6eaa3125fa99b9a92543b86f8271ab0ef4d31a
Mark R Pothecary <markpothers@hotmail.com>
2019-04-24 11:06:54 -0500

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
 // Code DelayedButton Component Here
+
+import React, {Component} from 'react'
+
+export default class CoordinatesButton extends Component {
+
+    // constructor(){
+    //     super()
+    // }
+
+    render (){
+        return(
+            <div>
+                <button onClick={ (event) =>{
+                    event.persist()
+                    const eventData = event
+                    setTimeout( () => this.props.onDelayedClick(eventData), this.props.delay )
+                }}>Button</button>
+            </div>
+        )
+    }
+
+}
